6722f96438969b3b503abe2b3a195aca611f5f70
Vadym Liubych <v.liubych@ukr.net>
2021-05-19 11:31:54 +0300

    Updates

---
+++ precache-manifest.81862ef0c2ac9cfb00b2d555122c2ed8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a6bbfd40600d96f151c2d754b46480",
+    "url": "./index.html"
+  },
+  {
+    "revision": "df171c63963d73b20efd",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "1aaf9929eaddc48efef1",
+    "url": "./static/css/main.27a20cf5.chunk.css"
+  },
+  {
+    "revision": "df171c63963d73b20efd",
+    "url": "./static/js/2.cd70d490.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cd70d490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aaf9929eaddc48efef1",
+    "url": "./static/js/main.47abc286.chunk.js"
+  },
+  {
+    "revision": "7859211cf5cc2a17445c",
+    "url": "./static/js/runtime-main.2d746ae6.js"
+  }
+]);
